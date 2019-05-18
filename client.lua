9ed0b8511fbd5f9faf7c5d2edccd8a71b71e39fc
FAXES <31368070+FAXES@users.noreply.github.com>
2019-05-18 23:02:58 +1000

    Formatting and a couple changes.

--- client.lua
+++ client.lua
@@ -1,4 +1,4 @@
-RegisterCommand("unrack", function(source, args)       
+RegisterCommand("unrack", function(source, args, rawCommand)       
         GiveWeaponToPed(GetPlayerPed(-1), GetHashKey("WEAPON_CARBINERIFLE"), 1000, false)
         GiveWeaponComponentToPed(GetPlayerPed(-1), GetHashKey("WEAPON_CARBINERIFLE"), GetHashKey("COMPONENT_AT_AR_FLSH"))
         GiveWeaponComponentToPed(GetPlayerPed(-1), GetHashKey("WEAPON_CARBINERIFLE"), GetHashKey("COMPONENT_AT_AR_AFGRIP"))
@@ -6,19 +6,20 @@ RegisterCommand("unrack", function(source, args)
 end)
 
 
-RegisterCommand("rack", function(source, args)
+RegisterCommand("rack", function(source, args, rawCommand)
 	local player = GetPlayerPed(-1) 
-RemoveWeaponFromPed(player, "WEAPON_CARBINERIFLE")
+	RemoveWeaponFromPed(player, "WEAPON_CARBINERIFLE")
 end)
 
+--------------------------------------------------
 
-RegisterCommand("unrack1", function(source, args) 
+RegisterCommand("unrack1", function(source, args, rawCommand) 
         GiveWeaponToPed(GetPlayerPed(-1), GetHashKey("WEAPON_PUMPSHOTGUN"), 1000, false)
-	    GiveWeaponComponentToPed(GetPlayerPed(-1), GetHashKey("WEAPON_PUMPSHOTGUN"), GetHashKey("COMPONENT_AT_AR_FLSH"))
+    	GiveWeaponComponentToPed(GetPlayerPed(-1), GetHashKey("WEAPON_PUMPSHOTGUN"), GetHashKey("COMPONENT_AT_AR_FLSH"))
 end)
 
 
-RegisterCommand("rack1", function(source, args)
+RegisterCommand("rack1", function(source, args. rawCommand)
 	local player = GetPlayerPed(-1) 
-RemoveWeaponFromPed(player, "WEAPON_PUMPSHOTGUN")
+	RemoveWeaponFromPed(player, "WEAPON_PUMPSHOTGUN")
 end)
